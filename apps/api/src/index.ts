b27d7f95c7a598f2122ef4fdbeab226d86bf9864
ryansldev <ryanlima1309@gmail.com>
2026-07-23 07:52:03 -0300

    feat: add cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,18 @@
-import { Elysia } from "elysia";
+import { cors } from "@elysia/cors";
+import { Elysia, env } from "elysia";
+import { auth } from "./infra/auth";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+if (!env.FRONTEND_URL) {
+  console.error("FRONTEND_URL is not set");
+  process.exit(1);
+}
+
+const app = new Elysia().use(cors({
+  origin: [env.FRONTEND_URL],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+})).mount(auth.handler).get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
