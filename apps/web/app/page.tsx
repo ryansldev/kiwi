79c1f277a58c759ceebe136008a838656c56ff57
ryansldev <ryanlima1309@gmail.com>
2026-07-22 23:12:12 -0300

    feat: add biome linting

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,10 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">🥝 Kiwi</h1>
-      <Button>
-        Get Started
-      </Button>
+      <Button>Get Started</Button>
     </div>
   );
 }
