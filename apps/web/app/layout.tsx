d4497f35ede30465e8a483a836292e011fdf3f09
ryansldev <ryanlima1309@gmail.com>
2026-07-23 01:37:42 -0300

    refactor: turn light mode the default

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="pt-BR"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">{children}</body>
     </html>
