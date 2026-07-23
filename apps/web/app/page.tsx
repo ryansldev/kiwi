c26dcfbcae8e5674a5811dec1380f75b15bf03f0
ryansldev <ryanlima1309@gmail.com>
2026-07-22 23:07:21 -0300

    feat: add shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">🥝 Kiwi</h1>
+      <Button>
+        Get Started
+      </Button>
     </div>
   );
 }
